1b5c32ea897904905cbb241ebf87b5640674e2e1
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-08-26 16:04:58 +0100

    Update local_directory.py

--- local_directory.py
+++ local_directory.py
@@ -1,1 +1,19 @@
 import os
+
+for name in os.listdir('.'):
+  
+  if os.path.isfile(name):
+    
+    print("File: ")
+  
+  elif os.path.isdir(name):
+    
+    print("Directory: ")
+    
+  elif os.path.islink(name):
+    
+    print("Link: ")
+    
+  else:
+    
+    print("Unknown: {}".format(name))
