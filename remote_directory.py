1ecda2f6d61129531b03fbef494947f1f4925112
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-11-22 13:14:20 +0000

    Update remote_directory.py

--- remote_directory.py
+++ remote_directory.py
@@ -1,13 +1,2 @@
 from ftplib import FTP
 
-def main():
-
-  address = input("Enter in the ftp server address: ")
-
-  path = input("Enter in the path of the directory inside the FTP server: ")
-
-  server = FTP(address)
-
-  server.login()
-
-  ftp.cwd(path)
