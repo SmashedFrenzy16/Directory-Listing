c419fa78708cd29576a3351673b2bbdd7731d381
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-11-23 12:10:05 +0000

    Update local_directory.py

--- local_directory.py
+++ local_directory.py
@@ -1,3 +1,18 @@
-import os
+from os import *
+
+direc = input("Enter in the path of the directory: ")
+
+contents = scandir(direc)
+
+for i in contents:
+
+    if i.is_file():
+
+        print(i.name)
+
+    elif i.is_dir():
+
+        print(f"/{i.name}")
+
 
 
