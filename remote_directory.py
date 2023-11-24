f1a752ac9e431f5964163373b753454268a0ee74
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-11-24 11:50:38 +0000

    Update remote_directory.py

--- remote_directory.py
+++ remote_directory.py
@@ -1,2 +1,23 @@
 from ftplib import FTP
 
+def get_ftp_connection(host, user, password):
+    ftp = FTP(host)
+    ftp.login(user, password)
+    return ftp
+
+def main():
+    
+    host = input("Enter in the FTP server name: ")
+    user = input("Enter in the FTP server username: ")
+    password = input("Enter in the FTP server password: ")
+    
+    ftp = get_ftp_connection(host, user, password)
+
+    direc = input("Enter in the name of a directory in the server: ")
+
+    ftp.cwd(direc)
+
+    ftp.retrlines('LIST')
+
+main()
+
